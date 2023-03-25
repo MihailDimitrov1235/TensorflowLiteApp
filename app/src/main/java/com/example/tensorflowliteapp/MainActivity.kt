549240784c86b5d477ff1b73638f332486b7f190
Misho <Mighty.strong1235@gmail.com>
2023-03-25 11:07:22 +0200

    permissions and model files

--- app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
+++ app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.tensorflowliteapp
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPermission();
+
     }
+
+    fun getPermission(){
+        val cameraPermision = android.Manifest.permission.CAMERA
+        if (ContextCompat.checkSelfPermission(this,cameraPermision) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(cameraPermision),101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermission();
+        }
+    }
+
 }
