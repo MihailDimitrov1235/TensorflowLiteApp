894cedf8ad2c2cd8f5675a3d84e8d449d92727c3
Misho <Mighty.strong1235@gmail.com>
2023-03-25 11:45:00 +0200

    added camera manager

--- app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
+++ app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
@@ -1,46 +1,57 @@
 package com.example.tensorflowliteapp
 
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.TextureView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textureView:TextureView
+    lateinit var cameraManager:CameraManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getPermission()
 
-        val textureView = findViewById<TextureView>(R.id.textureView)
-//        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
-//            override fun onSurfaceTextureAvailable(
-//                surface: SurfaceTexture,
-//                width: Int,
-//                height: Int
-//            ) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onSurfaceTextureSizeChanged(
-//                surface: SurfaceTexture,
-//                width: Int,
-//                height: Int
-//            ) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
-//                TODO("Not yet implemented")
-//            }
-//        }
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    fun openCamera(){
 
     }
 
