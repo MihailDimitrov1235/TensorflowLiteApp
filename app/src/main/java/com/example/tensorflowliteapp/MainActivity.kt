3d8c8dca45ab4e3264a582965bc34c80ee29167e
Misho <Mighty.strong1235@gmail.com>
2023-03-25 15:21:49 +0200

    working camera

--- app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
+++ app/src/main/java/com/example/tensorflowliteapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.tensorflowliteapp
 
+import android.annotation.SuppressLint
+import android.content.AsyncQueryHandler
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
 import android.view.TextureView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat.requestPermissions
@@ -13,11 +20,17 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
     lateinit var textureView:TextureView
     lateinit var cameraManager:CameraManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = (Handler(handlerThread.looper))
 
         getPermission()
 
@@ -44,15 +57,42 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
-                TODO("Not yet implemented")
             }
         }
 
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
     }
 
+    @SuppressLint("MissingPermission")
     fun openCamera(){
+        cameraManager.openCamera(cameraManager.cameraIdList[0],object:CameraDevice.StateCallback(){
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+                var surfaceTexture = textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
 
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        session.setRepeatingRequest(captureRequest.build(),null,null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        TODO("Not yet implemented")
+                    }
+                }, handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                TODO("Not yet implemented")
+            }
+        },handler)
     }
 
     fun getPermission(){
